aec86928f35fb2b3454dfb587ad440daffc924b2
Rahul Gururaj <rahul.gururaj96@gmail.com>
2020-11-09 18:51:50 +1100

    Drag and drop has a ghost

--- src/app/components/cell/cell.component.ts
+++ src/app/components/cell/cell.component.ts
@@ -1,5 +1,5 @@
 import {Component, ElementRef, Input, OnInit, ViewChild} from '@angular/core';
-import {Cell} from '../../models/cell';
+import {Cell, CellType} from '../../models/cell';
 import {gsap} from 'gsap';
 import {BrushService} from '../../services/brush.service';
 
@@ -17,6 +17,7 @@ export class CellComponent implements OnInit {
   //For animations
   visited: boolean = false;
   path: boolean = false;
+  placeholder: CellType;
 
   @ViewChild('tile', {static: true}) tile: ElementRef;
 
@@ -52,38 +53,66 @@ export class CellComponent implements OnInit {
 
   mouseOver() {
     if (this.brushService.mouseDown) {
-      if (this.cell.type !== 'start'
-        && this.cell.type !== 'end'
-        && this.brushService.brushSelected !== 'start'
-        && this.brushService.brushSelected !== 'end') {
+      if (!this.isWaypoint(this.cell.type)
+        && !this.isWaypoint(this.brushService.brushSelected)) {
         this.cell.type = this.brushService.brushSelected;
-      } else {
 
+      } else if (this.isWaypoint(this.brushService.brushSelected)) {
+        this.placeholder = this.brushService.brushSelected;
       }
     }
   }
 
+  mouseOut() {
+    if (this.brushService.mouseDown && this.isWaypoint(this.brushService.brushSelected)) {
+      this.placeholder = null;
+      // if (this.isWaypoint(this.cell.type)) {
+      //
+      // }
+    }
+  }
+
   onClick() {
-    if (this.cell.type !== 'start'
-      && this.cell.type !== 'end'
-      && this.brushService.brushSelected !== 'start'
-      && this.brushService.brushSelected !== 'end')
+    if (!this.isWaypoint(this.cell.type) && !this.isWaypoint(this.brushService.brushSelected)) {
       this.cell.type = this.brushService.brushSelected;
-    else if (this.cell.type == 'start'
-      || this.cell.type == 'end'){
-      console.log(`onclick reached`)
+    }
+    else if (this.isWaypoint(this.cell.type)) {
       this.brushService.brushSelected = this.cell.type;
       this.brushService.prevCell = this.cell;
     }
   }
 
   mouseUp() {
-    if (this.cell.type !== 'start' && this.cell.type !== 'end'
-      && (this.brushService.brushSelected == 'start' || this.brushService.brushSelected == 'end')) {
-      this.cell.type = this.brushService.brushSelected;
-      this.brushService.prevCell.type = 'clear';
-      this.brushService.brushSelected = 'wall';
+    if (this.isWaypoint(this.brushService.brushSelected)) {
+      if (this.waypointBeMovedHere()) {
+        this.cell.type = this.brushService.brushSelected;
+        this.brushService.prevCell.type = 'clear';
+        this.brushService.brushSelected = 'wall';
+        this.placeholder = null;
+
+      } else {
+        this.placeholder = null;
+      }
     }
+    // if (!this.isWaypoint(this.cell.type) && this.isWaypoint(this.brushService.brushSelected)) {
+    //
+    // }
+  }
+
+  isWaypoint(type: CellType) {
+    return type == 'start' || type == 'end';
+  }
+
+  waypointBeMovedHere() {
+    if (this.isWaypoint(this.cell.type))
+      return false;
+    return this.cell.type != 'wall';
+  }
+
+  get tileToDisplay() {
+    if (this.waypointBeMovedHere())
+      return this.placeholder || this.cell.type;
+    return this.cell.type;
   }
 
 }
