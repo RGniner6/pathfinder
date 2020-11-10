59a04e2a0366ab4eb9fdd33a2abb1ff4c371ace4
Rahul Gururaj <rahul.gururaj96@gmail.com>
2020-11-10 20:08:00 +1100

    Moved buttons to brush panel

--- src/app/components/controls/brush-panel/brush-panel.component.ts
+++ src/app/components/controls/brush-panel/brush-panel.component.ts
@@ -1,17 +1,20 @@
 import { Component, OnInit } from '@angular/core';
-import {Cell, CellType} from '../../models/cell';
-import {BrushService} from '../../services/brush.service';
+import {Cell, CellType} from '../../../models/cell';
+import {BrushService} from '../../../services/brush.service';
 import {BehaviorSubject} from 'rxjs';
+import {GridService} from '../../../services/grid.service';
 
 @Component({
   selector: 'app-brush-panel',
   templateUrl: './brush-panel.component.html',
-  styleUrls: ['./brush-panel.component.scss', '../cell/cell.component.scss']
+  styleUrls: ['./brush-panel.component.scss', '../../cell/cell.component.scss']
 })
 export class BrushPanelComponent implements OnInit {
   selectedBrush$: BehaviorSubject<CellType>;
   brushes: CellType[] = [];
-  constructor(private brushService: BrushService) {
+  constructor(private brushService: BrushService,
+              private gridService: GridService,
+  ) {
     this.brushes.push('clear', 'wall', 'weight');
     this.selectedBrush$ = new BehaviorSubject<CellType>('wall');
   }
@@ -22,6 +25,18 @@ export class BrushPanelComponent implements OnInit {
 
   selectBrush(cellType: CellType) {
     this.selectedBrush$.next(cellType);
+  }
+
+  clearObstacles() {
+    this.gridService.clearObstacles();
+  }
+
+  addRandomWalls() {
+    this.gridService.addRandomWalls(20);
+  }
+
+  resetGrid() {
+    this.gridService.refresh();
   }
 
 }
