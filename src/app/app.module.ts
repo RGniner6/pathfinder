95ff0e9af2ffd05357e55eb1761ee1a0d0efb6b2
Rahul Gururaj <rahul.gururaj96@gmail.com>
2020-11-10 20:00:59 +1100

    Changed positioning of component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,8 +5,8 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GridComponent } from './components/grid/grid.component';
 import { CellComponent } from './components/cell/cell.component';
-import { BrushPanelComponent } from './components/brush-panel/brush-panel.component';
-import { OptionsPanelComponent } from './components/options-panel/options-panel.component';
+import { BrushPanelComponent } from './components/controls/brush-panel/brush-panel.component';
+import { OptionsPanelComponent } from './components/controls/options-panel/options-panel.component';
 
 @NgModule({
   declarations: [
